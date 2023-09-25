35d281cde94b095e296a592c3f69e5aba5705f5d
rjhkim <73801105+rjhkim@users.noreply.github.com>
2023-09-25 16:47:33 -0400

    Time: 0 ms (100.00%) | Memory: 8.2 MB (16.76%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-
-        if (nums.size() == 1) {
-            vector<int> copy = nums;
-            vector<vector<int>> temp = {copy};
-            return temp;
+        int index = 0;
+        dfs(nums, ans, index);
+        return ans; 
+        }
+private:
+    void dfs(vector<int>& nums, vector<vector<int>>& ans, int index) {
+        if (index == nums.size()) {
+            ans.push_back(nums);
+            return;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            int temp = nums[0];
-            nums.erase(nums.begin());
-            vector<vector<int>> p = permute(nums);
-
-            for (int i = 0; i < p.size(); i++) {
-                p[i].push_back(temp);
-            }
-            for (int i = 0; i < p.size(); i++) {
-                ans.push_back(p[i]);
-            }
-            nums.push_back(temp);
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[index], nums[i]);
+            dfs(nums, ans, index + 1);
+            swap(nums[index], nums[i]);
         }
-
-        return ans;
-
-            
-        }
+    }
 };
