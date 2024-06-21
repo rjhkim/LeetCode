41f8ff9b81a75fd57850b4c8c055d527dcab3371
rjhkim <73801105+rjhkim@users.noreply.github.com>
2024-06-21 12:17:53 +0900

    Time: 62 ms (67.57%) | Memory: 29.1 MB (43.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,49 +2,67 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0) {
-                if (nums[i - 1] == nums[i]) {
-                    continue;
-                }
+        sort(nums.begin(), nums.end());
+        // Start the outer loop that will establish the goal
+        int size = nums.size();
+
+        if (size < 3) {
+            return ans;
+        }
+
+        int i = 0;
+
+        while (i < size - 2) {
+            // Skip duplicate elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                i++;
+                continue;
             }
-            int first = nums[i];
-            int second = i + 1;
-            int third = nums.size() - 1;
-            int target = 0;
-            while (second < third) {
-                int sum = first + nums[second] + nums[third];
-                if (sum == target) {
-                    vector<int> temp;
-                    temp.push_back(first);
-                    temp.push_back(nums[second]);
-                    temp.push_back(nums[third]);
+
+            int target = 0 - nums[i];
+            int left = i + 1;
+            int right = size - 1;
+
+            while (left < right) {
+                // If left and right pointers added equals target
+                if (nums[left] + nums[right] == target) {
+                    vector<int> temp = {nums[i], nums[left], nums[right]};
                     ans.push_back(temp);
 
-                    while (second < third && nums[second] == nums[second + 1]) {
-                        second++;
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;     
                     }
-                    second++;
-
-                    while (second < third && nums[third - 1] == nums[third]) {
-                        third--;
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
                     }
-                    third--;
+                    left++;
+                    right--;
                 }
-                else if (sum < target) {
-                    second++;
+                // Less than target
+                else if ((nums[left] + nums[right]) < target) {
+                    left++;
                 }
+                // Greater than target
                 else {
-                    third--;
+                    right--;
                 }
-
             }
+            i++;
         }
-        
-
         return ans;
-
     }
+
+    /*
+    Time Complexity:
+    1. Sorting: O(n log n)
+    2. Outer loop: O(n)
+    3. Inner loop: O(n) in the worst case
+
+    Therefore, the overall time complexity is O(n^2).
+
+    Space Complexity: O(1) (excludin
+
+*/
 };
