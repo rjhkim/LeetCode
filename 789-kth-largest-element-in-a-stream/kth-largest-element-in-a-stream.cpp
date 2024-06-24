befe001f5219e17461ed44833a856a81e94c6a47
rjhkim <73801105+rjhkim@users.noreply.github.com>
2024-06-24 12:36:12 +0900

    Time: 33 ms (13.02%) | Memory: 23.5 MB (33.96%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -1,31 +1,29 @@
 class KthLargest {
 public:
     KthLargest(int k, vector<int>& nums) {
-        this->k = k;
+        //add all values of nums into priority queue in decrease order, a min priority queue
+        //as you are adding, you delete / pop values if. itexceeds k
+        //push/pop priority queue = log(n)
+        //top access = O(1)
+        k_values = k;
         for (int i = 0; i < nums.size(); i++) {
             pq.push(nums[i]);
-        }
-        while (pq.size() > this->k) {
-            pq.pop();
+            if (pq.size() > k) {
+                pq.pop();
+            }
         }
     }
-    
     int add(int val) {
         pq.push(val);
-        if (pq.size() > k) {
+        if (pq.size() > k_values) {
             pq.pop();
         }
         return pq.top();
     }
-
 private:
-    int k;
+    int k_values;
     priority_queue<int, vector<int>, greater<int>> pq;
-
 };
-
-//use priority queues
-
 
 /**
  * Your KthLargest object will be instantiated and called as such:
